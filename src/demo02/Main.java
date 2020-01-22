53a5b0052c7ba0145f79e537a6f6c6c6b2c54d98
haschish <haschish@gmail.com>
2020-01-22 18:17:59 +0500

    fixed demo02

--- src/demo02/Main.java
+++ src/demo02/Main.java
@@ -1,7 +1,10 @@
 package demo02;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File f1 = new File("test.txt");
         File f2 = new File("./../io/simlink.txt");
         System.out.println(f1.getAbsolutePath());
